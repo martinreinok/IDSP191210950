01d40e1b8836dbc7fa6a3c4ae26f809c733512d3
Martin Reinok <martin.reinok@gmail.com>
2023-03-28 13:40:43 +0200

    TRA2

---
+++ Assignment2/TRA/cpp/tb_TRA2.cpp
@@ -0,0 +1,26 @@
+// -----------------------------------------------------------------------------
+// File         : tb_sec_df2.cpp
+// Description  : C++ testbench for sec_df2
+// Author       : Sabih Gerez, University of Twente
+// Creation date: March 14, 2019
+// -----------------------------------------------------------------------------
+// $Rev: 1$
+// $Author: gerezsh$
+// $Date: Mon Feb 20 23:22:13 CET 2023$
+// $Log$
+// -----------------------------------------------------------------------------
+
+
+#include "TRA2.h"
+
+// -----------------------------------------------------------------------------
+// parameters
+// -----------------------------------------------------------------------------
+
+#define TOP_DESIGN TRA2
+
+#define WORD_LENGTH 8
+#define INT_WORD_LENGTH 5
+#define CLOCKS_PER_SAMPLE 1
+
+#include "tb_sec_generic.cpp"
